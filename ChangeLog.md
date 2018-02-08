b5244fb2c43ef973852606e966aadfba1f2a8ff7
Joachim Breitner <mail@joachim-breitner.de>
2018-02-08 17:27:24 -0500

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for containers-verified
+
+## 0.5.11.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Data/IntSet.hs
@@ -0,0 +1,91 @@
+
+{-# OPTIONS_HADDOCK not-home #-}
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | Please see the documentation of <http://hackage.haskell.org/package/containers containers> for details.
+module Data.IntSet (
+--            -- * Strictness properties
+--            -- $strictness
+
+            -- * Set type
+              IntSet          -- instance Eq,Show
+            , Key
+
+            -- * Operators
+            , (\\)
+
+            -- * Query
+            , IS.null
+            , size
+            , member
+            , notMember
+--            , lookupLT
+--            , lookupGT
+--            , lookupLE
+--            , lookupGE
+            , isSubsetOf
+            , isProperSubsetOf
+--            , disjoint
+
+            -- * Construction
+            , empty
+            , singleton
+            , insert
+            , delete
+
+            -- * Combine
+            , union
+--            , unions
+            , difference
+            , intersection
+
+            -- * Filter
+            , IS.filter
+            , partition
+--            , split
+--            , splitMember
+--            , splitRoot
+
+            -- * Map
+--            , IS.map
+
+            -- * Folds
+            , IS.foldr
+            , IS.foldl
+            -- ** Strict folds
+            , foldr'
+            , foldl'
+            -- ** Legacy folds
+            , fold
+
+--            -- * Min\/Max
+--            , findMin
+--            , findMax
+--            , deleteMin
+--            , deleteMax
+--            , deleteFindMin
+--            , deleteFindMax
+--            , maxView
+--            , minView
+
+            -- * Conversion
+
+            -- ** List
+            , elems
+            , toList
+--            , fromList
+
+            -- ** Ordered list
+            , toAscList
+            , toDescList
+--            , fromAscList
+--            , fromDistinctAscList
+
+--            -- * Debugging
+--            , showTree
+--            , showTreeWith
+
+            ) where
+
+import "containers" Data.IntSet as IS
