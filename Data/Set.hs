5904c67134c92b99c18c6546d93cb17017197111
Joachim Breitner <mail@joachim-breitner.de>
2018-02-19 17:53:28 -0500

    Add a small bit of `Data.Set`

---
+++ Data/Set.hs
@@ -0,0 +1,97 @@
+
+module Data.Set (
+            -- * Set type
+              Set          -- instance Eq,Ord,Show,Read,Data,Typeable
+
+            -- * Operators
+--          , (\\)
+
+            -- * Query
+            , S.null
+--          , size
+            , member
+            , notMember
+--          , lookupLT
+--          , lookupGT
+--          , lookupLE
+--          , lookupGE
+--          , isSubsetOf
+--          , isProperSubsetOf
+--          , disjoint
+
+            -- * Construction
+            , empty
+            , singleton
+            , insert
+            , delete
+--          , powerSet
+
+            -- * Combine
+            , union
+--          , unions
+--          , difference
+--          , intersection
+--          , cartesianProduct
+--          , disjointUnion
+
+--          -- * Filter
+--          , S.filter
+--          , takeWhileAntitone
+--          , dropWhileAntitone
+--          , spanAntitone
+--          , partition
+--          , split
+--          , splitMember
+--          , splitRoot
+
+--          -- * Indexed
+--          , lookupIndex
+--          , findIndex
+--          , elemAt
+--          , deleteAt
+--          , S.take
+--          , S.drop
+--          , S.splitAt
+
+--          -- * Map
+--          , S.map
+--          , mapMonotonic
+
+--          -- * Folds
+--          , S.foldr
+--          , S.foldl
+--          -- ** Strict folds
+--          , foldr'
+--          , foldl'
+--          -- ** Legacy folds
+--          , fold
+
+--          -- * Min\/Max
+--          , lookupMin
+--          , lookupMax
+--          , findMin
+--          , findMax
+--          , deleteMin
+--          , deleteMax
+--          , deleteFindMin
+--          , deleteFindMax
+--          , maxView
+--          , minView
+
+--          -- * Conversion
+
+--          -- ** List
+--          , elems
+--          , toList
+--          , fromList
+
+--          -- ** Ordered list
+--          , toAscList
+--          , toDescList
+--          , fromAscList
+--          , fromDescList
+--          , fromDistinctAscList
+--          , fromDistinctDescList
+            ) where
+
+import Data.Set.Internal as S
